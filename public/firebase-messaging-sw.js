4e3756c13084427da53b24a2c66b5536d9ae151a
FredEspinosa <alfredodicio490@gmail.com>
2025-06-07 23:28:50 +0100

    (Alfredo) Limpieza de comentarios de variables

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,14 +2,6 @@
 importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
 
-// const firebaseApyKey = import.meta.env.VITE_HELENA_FIREBASE_APKY;
-// const firebaseAuhtDomain =import.meta.env.VITE_HELENA_FIREBASE_AUTH_DOMAIN;
-// const firebaseProjectId =import.meta.env.VITE_HELENA_FIREBASE_PROJECT_ID;
-// const firebaseStorageBucket = import.meta.env.VITE_HELENA_FIREBASE_STORAGE_BUCKET;
-// const firebaseMessaginSenderId = import.meta.env.VITE_HELENA_FIREBASE_MESSAGING_SENDER_ID;
-// const firebaseAppId = import.meta.env.VITE_HELENA_FIREBASE_APP_ID;
-// const firebaseMeasuramentId = import.meta.env.VITE_HELENA_FIREBASE_MEASUREMENT_ID;
-
 // firebase.initializeApp({
 //   apiKey: "AIzaSyBw3PfxvKcXF4USNMA4jN7uYAUM4l7dbiQ",
 //   authDomain: "helenaapp-75fdb.firebaseapp.com",
@@ -18,16 +10,6 @@ importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-comp
 //   messagingSenderId: "1088597517320",
 //   appId: "1:1088597517320:web:9f7573c5316f83d6e99dbd",
 //   measurementId: "G-MSSGEF52TS"
-// });
-
-// firebase.initializeApp({
-//   apiKey: firebaseApyKey,
-//   authDomain: firebaseAuhtDomain,
-//   projectId: firebaseProjectId,
-//   storageBucket: firebaseStorageBucket,
-//   messagingSenderId: firebaseMessaginSenderId,
-//   appId: firebaseAppId,
-//   measurementId: firebaseMeasuramentId,
 // });
 
 firebase.initializeApp({
--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -14,24 +14,6 @@ const firebaseMessaginSenderId = import.meta.env.VITE_HELENA_FIREBASE_MESSAGING_
 const firebaseAppId = import.meta.env.VITE_HELENA_FIREBASE_APP_ID;
 const firebaseMeasuramentId = import.meta.env.VITE_HELENA_FIREBASE_MEASUREMENT_ID;
 
-// console.log("firebaseApyKey", firebaseApyKey);
-// console.log("firebaseAuhtDomain", firebaseAuhtDomain);
-// console.log("firebaseProjectId", firebaseProjectId);
-// console.log("firebaseStorageBucket", firebaseStorageBucket);
-// console.log("firebaseMessaginSenderId", firebaseMessaginSenderId);
-// console.log("firebaseAppId", firebaseAppId);
-// console.log("firebaseMeasuramentId", firebaseMeasuramentId);
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBw3PfxvKcXF4USNMA4jN7uYAUM4l7dbiQ",
-//     authDomain: "helenaapp-75fdb.firebaseapp.com",
-//     projectId: "helenaapp-75fdb",
-//     storageBucket: "helenaapp-75fdb.firebasestorage.app",
-//     messagingSenderId: "1088597517320",
-//     appId: "1:1088597517320:web:9f7573c5316f83d6e99dbd",
-//     measurementId: "G-MSSGEF52TS"
-// };
-
 const firebaseConfig = {
     apiKey: firebaseApyKey,
     authDomain: firebaseAuhtDomain,
